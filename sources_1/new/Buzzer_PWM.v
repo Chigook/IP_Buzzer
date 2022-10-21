261385222f8003401ee87f5f454f9afee0709ef6
Chigook <leechi93@naver.com>
2022-10-21 16:18:55 +0900

    Second Buzzer PWM

--- sources_1/new/Buzzer_PWM.v
+++ sources_1/new/Buzzer_PWM.v
@@ -15,7 +15,7 @@ module Buzzer_PWM(
     assign o_clk = r_clk;
 
     always @(posedge i_clk or posedge i_reset) begin
-        if(!i_reset | i_freqPWM == 0)begin
+        if(!i_reset || i_freqPWM == 0)begin
             r_counter <= 0;
             r_clk <= 0;
         end
